60d7b707f924cfcc3df0569ea5e51d25e8b0816d
khuong.2906 <khuongtran.2906@hcmut.edu.vn>
2024-09-19 12:12:21 +0700

    ````` `

--- main.py
+++ main.py
@@ -7,70 +7,63 @@ try:
     import os
     import time
     from discord_webhook import DiscordWebhook, DiscordEmbed
+    import pygetwindow as gw
 except ImportError:
     os.system("pip install pillow opencv-python discord-webhook pygetwindow pyautogui")
+
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.StreamHandler()
-    ]
+    handlers=[logging.StreamHandler()]
 )
+
+logging.warning("Remember, your UI Scale must be 1 in Anime Vanguards!")
 
 with open("config.json", "r") as config_file:
     config = json.load(config_file)
-show_full_screen = config.get("screenshot_full_screen",False)
 
-def send_webhook(webhook_url, embed, content, screenshot):
+show_full_screen = config.get("screenshot_full_screen", False)
+player_ids = config.get("player_id_to_ping", [])
+checkcooldown = config.get("resend_timeout", 100)
+send_webhook_delay = config.get("send_webhook_delay", 0.5)
+screenshot_delay = config.get("screenshot_delay", 0.5)
+
+player_ping_string = " ".join([f"<@{i}>" for i in player_ids]) if config.get("ping_players", False) else ""
+logging.info(f"Players to ping: '{player_ping_string}'")
+
+def send_webhook(webhook_url, embed, content, screenshot_bytes=None):
     webhook = DiscordWebhook(webhook_url, content=content)
     webhook.add_embed(embed)
     embed.set_thumbnail(url="https://static.wikia.nocookie.net/rbx-anime-vanguards/images/7/71/AnimeVanguards.png/revision/latest?cb=20240531035940")
     embed.set_timestamp()
-    if screenshot:
-        with open("screenshot.png", 'rb') as f:
-            webhook.add_file(file=f.read(), filename='screenshot.png')
+
+    if screenshot_bytes:
+        webhook.add_file(file=screenshot_bytes, filename='screenshot.png')
         embed.set_image(url="attachment://screenshot.png")
 
     response = webhook.execute()
-    if screenshot:
-        os.remove('screenshot.png')
     logging.info("Webhook message sent successfully.")
-if show_full_screen == False:
-    import pygetwindow as gw
-
 
 def capture_window(window_title):
     try:
         if show_full_screen:
-            screenshot = ImageGrab.grab()
-        else:
-            roblox_window = next((win for win in gw.getWindowsWithTitle(window_title)), None)
-            if roblox_window and roblox_window.isActive:
-                logging.info("Found Roblox Window!")
-                bbox = (roblox_window.left + 10, roblox_window.top + 10, roblox_window.right - 10, roblox_window.bottom - 10)
-                screenshot = ImageGrab.grab(bbox)
-            else:
-                logging.warning(f"Window titled '{window_title}' not found. Using placeholder image.")
-                screenshot = Image.open('placeholder.png')
-        return screenshot
+            return ImageGrab.grab()
+
+        roblox_window = next((win for win in gw.getWindowsWithTitle(window_title)), None)
+        if roblox_window and roblox_window.isActive:
+            logging.info("Found Roblox Window!")
+            bbox = (roblox_window.left + 10, roblox_window.top + 10, roblox_window.right - 10, roblox_window.bottom - 10)
+            return ImageGrab.grab(bbox)
+        
+        logging.warning(f"Window titled '{window_title}' not found. Using placeholder image.")
+        return Image.open('placeholder.png')
+    
     except Exception as e:
         logging.error(f"Error capturing window '{window_title}': {e}. Using placeholder image.")
         return Image.open('placeholder.png')
 
-player_ping_string = ""
-player_ids = config.get("player_id_to_ping", [])
-for i in player_ids:
-    player_ping_string = f"{player_ping_string}<@{i}> "
-if not config.get("ping_players", False):
-    player_ping_string = ""
-logging.info(f"Players to ping: '{player_ping_string}'")
-
 image_prototypes = ["replay1.png", "replay2.png", "return1.png", "return2.png"] 
-checkcooldown = config.get("resend_timeout", 100)
-send_webhook_delay = config.get("send_webhook_delay",0.5)
-screenshot_delay = config.get("screenshot_delay",0.5)
-
 last_webhook_sent = 0
 webhooks_sent = 1
 
@@ -80,53 +73,44 @@ while True:
             button_location = pyautogui.locateCenterOnScreen(image_file, confidence=0.75)
             if button_location:
                 logging.info(f"Found image: '{image_file}' at {button_location}")
-                try:
-                    time.sleep(screenshot_delay)
-                    screenshot = capture_window("Roblox")
-                    if screenshot:
-                        screenshot_path = 'screenshot.png'
-                        if os.path.exists(screenshot_path):
-                            os.remove(screenshot_path)
-                        
-                        image_bytes = BytesIO()
-                        screenshot.save(image_bytes, format='PNG')
-                        image_bytes.seek(0)
+                
+                time.sleep(screenshot_delay)
+                screenshot = capture_window("Roblox")
+                screenshot_bytes = BytesIO()
+                screenshot.save(screenshot_bytes, format='PNG')
+                screenshot_bytes.seek(0)
 
-                        with open(screenshot_path, 'wb') as f:
-                            f.write(image_bytes.getvalue())
-                    description_text = f"``` ```\n> # Map Finished!\n> ## {webhooks_sent} Webhook Update{'s' if webhooks_sent != 1 else ''} Sent!"
+                description_text = f"``` ```\n> # Map Finished!\n> ## {webhooks_sent} Webhook Update{'s' if webhooks_sent != 1 else ''} Sent!"
 
-                    if webhooks_sent != 1:
-                        time_now = time.time()  
-                        time_elapsed = time_now - last_webhook_sent 
-                        minutes, seconds = divmod(time_elapsed, 60)
-    
-                        logging.info(f"Time since last update: {int(minutes)} minutes, {seconds:.2f} seconds")
-                        description_text = f"{description_text}\n> ### Time since last update: {int(minutes)} minutes, {seconds:.2f} seconds."
-    
-                        last_webhook_sent = time_now  
-                    else:
-                        last_webhook_sent = time.time()
+                if webhooks_sent > 1:
+                    time_now = time.time()  
+                    time_elapsed = time_now - last_webhook_sent
+                    minutes, seconds = divmod(time_elapsed, 60)
+                    description_text += f"\n> ### Time since last update: {int(minutes)} minutes, {seconds:.2f} seconds."
 
+                last_webhook_sent = time.time()
 
-                    embed = DiscordEmbed(
-                        title="Game Finish UI Spotted!",
-                        description=description_text
-                    )
-                    
-                    
-                    logging.info("Found Finished Game UI, making Discord Embed!")
-                    time.sleep(send_webhook_delay)
-                    for webhook_url in config.get("discord_webhook", []):
-                        send_webhook(webhook_url, embed, player_ping_string, screenshot)
-                    webhooks_sent += 1
-                    logging.info(f"Waiting {checkcooldown}s before sending another message.")
-                    time.sleep(checkcooldown) 
-                except Exception as img_error:
-                    logging.error(f"Error capturing or sending screenshot: {img_error}")
+                embed = DiscordEmbed(
+                    title="Game Finish UI Spotted!",
+                    description=description_text
+                )
+
+                logging.info("Found Finished Game UI, creating Discord Embed.")
+                time.sleep(send_webhook_delay)
+                
+                for webhook_url in config.get("discord_webhook", []):
+                    send_webhook(webhook_url, embed, player_ping_string, screenshot_bytes.getvalue())
+                
+                webhooks_sent += 1
+                logging.info(f"Waiting {checkcooldown}s before sending another message.")
+                time.sleep(checkcooldown)
+                
             time.sleep(0.1)
-    except Exception as er:
-        logging.error(f"Error: {er}")
+    
+    except pyautogui.ImageNotFoundException:
+        logging.error("Could not find image!")
+    except Exception as e:
+        logging.error(f"Unexpected error: {e}")
         time.sleep(1)
     
     time.sleep(0.1)
